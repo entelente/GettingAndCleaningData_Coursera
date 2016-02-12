d896c3306d088c6207df9634090f3cddef3f2df1
Janos Strasszer <little.evil.froggy@gmail.com>
2016-02-12 10:11:29 +0100

    Created project, added ignores to .gitignore

---
+++ run_analysis.R
@@ -0,0 +1,5 @@
+# This script is the result of the final assignment for the Getting and Cleaning Data - Coursera course
+# Please read additional details in CodeBook.md
+
+# Important: This script assumes that data is downloaded and in the working directory.
+
